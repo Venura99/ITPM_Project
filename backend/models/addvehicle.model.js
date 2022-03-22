4cb2f597a04f2fc9eed1f557805295ef995bcf8a
IT20159580 <venurageethma99@gmail.com>
2022-03-22 10:36:17 +0530

    https://github.com/Venura99/ITPM_Project.git

---
+++ backend/models/addvehicle.model.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const addvehicleSchema = new Schema({
+    _id: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+
+    },
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    vehicleImage: {
+        type: String,
+        required: true,
+    },
+    price: {
+        type: Number,
+
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+}, {
+    timestamps: true,
+})
+
+const Addvehicle = mongoose.model("Addvehicle", addvehicleSchema);
+
+module.exports = Addvehicle;
